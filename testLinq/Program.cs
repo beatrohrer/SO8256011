ce4a2369a6afab948275e30141e5c943398b2c68
Beat Rohrer <beat.rohrer@zhdk.ch>
2019-02-04 10:33:42 +0100

    Init

---
+++ testLinq/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testLinq
+{
+	static class Program
+	{
+		static void Main(string[] args)
+		{
+			var db = new DataClasses1DataContext(@"");
+			//var b = db.GetNextSequenceValue();
+			var testTableRecord1 = new testTable()
+			{
+				a = 1,
+			};
+
+			var testTableRecord2 = new testTable()
+			{
+				a = 2,
+			};
+
+			db.Connection.Open();
+			db.testTables.InsertOnSubmit(testTableRecord1);
+			db.SubmitChanges();
+			
+			db.testTables.InsertOnSubmit(testTableRecord2);
+			db.SubmitChanges();
+			Console.WriteLine($"id1: {testTableRecord1.id}");
+			Console.WriteLine($"id2: {testTableRecord2.id}");
+		}
+	}
+}
